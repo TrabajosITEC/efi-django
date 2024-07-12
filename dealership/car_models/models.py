1271899e649467711fbc68ac29b26493d2aa9051
marcosIrazoquiITEC <m.irazoqui@itecriocuarto.org.ar>
2024-07-12 19:07:22 -0300

    primeros models

--- dealership/car_models/models.py
+++ dealership/car_models/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from brands.models import Brand
 
-# Create your models here.
+class CarModel(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
--- dealership/cars/models.py
+++ dealership/cars/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from brands.models import Brand
+from car_models.models import CarModel
+from categories.models import Category
 
-# Create your models here.
+class Car(models.Model):
+    brand = models.ForeignKey(Brand,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    car_model = models.ForeignKey(CarModel,on_delete=models.CASCADE)
+    year = models.IntegerField()
+    price = models.DecimalField()
+
+    def __str__(self):
+        return f'{self.brand.name} {self.car_model.name} ({self.year})'
