7eb4a907aa2a99fc6dc5abf392df4a3ac3e882c7
juantobares4 <j.tobares@itecriocuarto.org.ar>
2024-07-21 19:24:10 -0300

    create forms

---
+++ dealership/locations/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from locations.models import(
+    Country,
+    Province,
+    Locality
+
+)
+
+class CountryForm(forms.ModelForm):
+    class Meta:
+        model = Country
+
+        fields = [
+            'name',
+
+        ]
+
+        widgets = {
+            'name': forms.TextInput(attrs = {'class': 'form-control'})
+
+        }
+
+class ProvinceForm(forms.ModelForm):
+    class Meta:
+        model = Province
+
+        fields = [
+            'name',
+            'country'
+
+        ]
+
+        widgets = {
+            'name': forms.TextInput(attrs = {'class': 'form-control'}),
+            'country': forms.Select(attrs = {'class': 'form-control'})
+
+        }
+
+class LocalityForm(forms.ModelForm):
+    class Meta:
+        model = Locality
+
+        fields = [
+            'name',
+            'province'
+
+        ]
+
+        widgets = {
+            'name': forms.TextInput(attrs = {'class': 'form-control'}),
+            'province': forms.Select(attrs = {'class': 'form-control'})
+
+        }
