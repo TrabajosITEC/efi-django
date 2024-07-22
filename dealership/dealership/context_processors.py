5d699ad31f35d85e39b744f7152d93c0bc44a045
marcosIrazoquiITEC <m.irazoqui@itecriocuarto.org.ar>
2024-07-22 20:37:41 -0300

    app cars

---
+++ dealership/dealership/context_processors.py
@@ -0,0 +1,8 @@
+from cars.models import Category 
+
+def all_names_categories(request):
+    categories = Category.objects.all()
+    names = [category.name for category in categories]
+    return dict(
+        names_categories = names
+    )
