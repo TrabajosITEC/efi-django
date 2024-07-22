a60ed6a6b4e4accf6152615147347ce04bc02bc7
juantobares4 <j.tobares@itecriocuarto.org.ar>
2024-07-21 21:57:57 -0300

    .

--- dealership/locations/urls.py
+++ dealership/locations/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 
 from locations.views.country_views import (
-    CountryList    
+    CountryList,
+    CountryCreate,
+    CountryUpdate,
+    CountryDetail,
+    CountryDelete
 
 )
 
 urlpatterns = [
-    path(route = 'country_list/', view = CountryList.as_view(), name = 'country_list'),
+    path(route = 'countries_list/', view = CountryList.as_view(), name = 'countries_list'),
+    path(route = 'country_create/', view = CountryCreate.as_view(), name = 'country_create'),
+    path(route = 'country_update/<int:id>/', view = CountryUpdate.as_view(), name = 'country_update'),
+    path(route = 'country_detail/<int:id>/', view = CountryDetail.as_view(), name = 'country_detail'),
+    path(route = 'country_delete/<int:id>/', view = CountryDelete.as_view(), name = 'country_delete')
 
 ]
--- dealership/locations/views/country_views.py
+++ dealership/locations/views/country_views.py
@@ -26,3 +26,79 @@ class CountryList(View):
             )
 
         )
+
+class CountryCreate(View):
+    def get(self, request):
+        country_form = CountryForm()
+
+        return render(request, 
+            'country/create.html',
+            dict(
+                form = country_form
+                
+                )
+
+        )
+
+    def post(self, request):
+        country_form = CountryForm(request.POST)
+
+        if country_form.is_valid():
+            new_country = country_repository.create(
+                name = country_form.cleaned_data['name']
+
+            )
+
+            return redirect('countries_list')
+
+class CountryUpdate(View):
+    def get(self, request, id):
+        get_country = country_repository.get_by_id(id = id)
+        country_form = CountryForm(instance = get_country)
+
+        return render(
+            request,
+            'country/update.html',
+            dict(
+                form = country_form
+
+            )
+
+        )
+    
+    def post(self, request, id):
+        filter_country = country_repository.get_by_id(id = id)
+        country_form = CountryForm(request.POST, instance = filter_country)
+
+        if country_form.is_valid():
+            name = country_form.cleaned_data['name']
+
+            country_repository.update(
+                country = filter_country,
+                name = name    
+            
+            )
+
+            return redirect('country_detail', filter_country.id)
+
+class CountryDelete(View):
+    def get(self, request, id):
+        clicked_country_to_delete = country_repository.get_by_id(id = id)
+
+        country_repository.delete(country = clicked_country_to_delete)
+
+        return redirect('countries_list')
+
+class CountryDetail(View):
+    def get(self, request, id):
+        filter_country = country_repository.get_by_id(id = id)
+
+        return render(
+            request, 
+            'country/detail.html',
+            dict(
+                country = filter_country
+
+            )
+
+        )
