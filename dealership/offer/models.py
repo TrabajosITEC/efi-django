a138e3724b3ead5856ef8860d5b6f485d5c8a326
Joaquin Martinez <jo.martinez@itecriocuarto.org.ar>
2024-07-22 21:18:41 -0300

    creacion app ofertas

---
+++ dealership/offer/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Offer(models.Model):
+    ...
+
+class OfferImage(models.Model):
+    ...
+
+class OfferPayment(models.Model):
+    ...
+
